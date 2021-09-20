bd4a26f1ca9c566eb2e7023aa010fa1c924c829a
wsmagacki <waldemar.smagacki@gmail.com>
2021-09-21 00:02:02 +0200

    aaa

--- src/main/java/com/example/model/TaskRepository.java
+++ src/main/java/com/example/model/TaskRepository.java
@@ -2,7 +2,16 @@ package com.example.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
-@RepositoryRestResource
+@RepositoryRestResource(path = "todos", collectionResourceRel = "todos2")
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(Long aLong);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Task task);
 }
